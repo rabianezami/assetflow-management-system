9074629e1a03aff1c3edd642cca8443db653ebae
rabia zia nezami <rabianezami786@gmail.com>
2026-08-02 15:00:08 +0430

    feat: add asset database schemas and migrations

---
+++ packages/db/src/schema/index.ts
@@ -0,0 +1,7 @@
+export { assetLifecycleEnum, assetStatusEnum } from "./enums";
+export {
+  assetTypes,
+  type AssetTypeRow,
+  type NewAssetTypeRow,
+} from "./asset-types";
+export { assets, type AssetRow, type NewAssetRow } from "./assets";
